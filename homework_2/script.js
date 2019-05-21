ff36a4e6e0482f31a9b14ff043b3eb290726a4b0
Dima <cruselnitschiid.@mail.ru>
2019-05-21 11:07:57 +0300

    Домашняя работа 3

--- homework_2/script.js
+++ homework_2/script.js
@@ -1,7 +1,7 @@
 'use strict';
 
 let money = +prompt("Ваш бюджет на месяц?", ''),
-    time = prompt("Введите дату в формате YYYY-MM-DD", "YYYY-MM-DD", '');
+    time = prompt("Введите дату в формате YYYY-MM-DD", "YYYY-MM-DD");
 
 let appData = {
     budget: money,
